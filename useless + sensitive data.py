a58e1ca4d6c41fbadbd05c99014c519ae4923690
I.Did.It.Rizz <ididitrizz@gmail.com>
2026-02-19 15:12:31 -0500

    Delete useless thing

--- useless + sensitive data.py
+++
@@ -1,9 +0,0 @@
-print("Cows eat monkeys")
-sensitivedata = {"name": "James Shirefeld", "Social Security #": "132-53-6421", "Credit Card Number": "1324 4586 5364 5732", "Address": "432 Ridge St, OH, USA"}
-#DO NOT LEAK THIS DATA
-def protectdata():
-  repetition = 0
-  while repetition < 200:
-    print(sensitivedata)
-    repetition = repetition + 1
-print("oops")
